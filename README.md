41b899a866d9541c639bae30f9f5b32cdb0f3535
AlexVipond <alex.r.vipond@gmail.com>
2019-12-24 08:50:03 -0600

    migrate scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# `@baleada/listenable-gestures`
+
+A collection of gesture recognizers that are compatible with Baleada Logic's Listenable class.
---
+++ index.js
@@ -0,0 +1,9 @@
+const generateIndex = require('./lib/generateIndex'),
+      empty = require('./lib/empty'),
+      exec = require('./lib/exec')
+
+module.exports = {
+  generateIndex,
+  empty,
+  exec
+}
