68a2746f262d378124550254ca2fc745ae96bf44
AlexVipond <alex.r.vipond@gmail.com>
2020-12-25 19:42:25 -0600

    add tab shortcut

--- src/withPuppeteer.js
+++ src/withPuppeteer.js
@@ -15,9 +15,22 @@ export default function withPuppeteer (suite, options = {}) {
           mouseClick = async selector => {
             const coords = await page.evaluate(selector => JSON.parse(JSON.stringify(document.querySelector(selector).getBoundingClientRect())), selector)
             await page.mouse.click(coords.x, coords.y)
+          },
+          tab = async ({ direction, total }) => {
+            const iterable = (new Array(total)).fill()
+            switch (direction) {
+              case 'forward':
+                iterable.forEach(async () => await page.keyboard.press('Tab'))
+                break
+              case 'backward':
+                await page.keyboard.down('Shift')
+                iterable.forEach(async () => await page.keyboard.press('Tab'))
+                await page.keyboard.up('Shift')
+                break
+            }
           }
 
-    context[contextKey] = { browser, page, mouseClick }
+    context[contextKey] = { browser, page, mouseClick, tab }
   })
   
   suite.after(async context => {
