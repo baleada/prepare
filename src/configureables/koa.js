bc1a1133e86c90c2dd3c64c63d0cc9d61594c81f
AlexVipond <alex.r.vipond@gmail.com>
2020-12-30 11:46:14 -0600

    support icon stuff in koa

--- src/configureables/koa.js
+++ src/configureables/koa.js
@@ -3,6 +3,8 @@ import asVue from '@baleada/vite-serve-as-vue'
 import createFilesToIndex from '@baleada/source-transform-files-to-index'
 import createFilesToRoutes from '@baleada/source-transform-files-to-routes'
 import testable from '../testable.js'
+import toIconComponent from '../util/toIconComponent.js'
+import toIconComponentIndex from '../util/toIconComponentIndex.js'
 
 export default function configureable (config = []) {
   const object = {}
@@ -32,6 +34,17 @@ export default function configureable (config = []) {
       transform: createFilesToRoutes(router, { test: () => true, importType: 'relativeFromRoot', ...createFilesToRoutesOptions })
     }))
   }
+  object.virtual.iconComponentIndex = ({ icons }) => object.virtual(({ testable }) => ({
+    test: testable.idEndsWith('src/index.js').test,
+    transform: () => toIconComponentIndex({ icons })
+  }))
+  object.virtual.iconComponents = ({ icons }) => object.virtual(({ testable }) => ({
+    test: param => 
+    icons.some(({ componentName }) => testable.idEndsWith(`src/components/${componentName}.vue`).test(param))
+    &&
+    testable.queryIsEmpty().test(param),
+    transform: ({ id }) => toIconComponent(icons.find(({ componentName }) => id.endsWith(`${componentName}.vue`)))
+  }))
 
   return object
 }
