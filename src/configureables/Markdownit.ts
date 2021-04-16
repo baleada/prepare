e39f8d210d969500fb1d3c33824fd4aebb76e485
AlexVipond <alex.r.vipond@gmail.com>
2021-04-16 14:40:00 -0600

    adds Markdownit.ts

---
+++ src/configureables/Markdownit.ts
@@ -0,0 +1,88 @@
+import MarkdownIt from 'markdown-it'
+// @ts-ignore
+import proseContainer from '@baleada/markdown-it-prose-container'
+// @ts-ignore
+import spaLinks from '@baleada/markdown-it-spa-links'
+// @ts-ignore
+import textContent from '@baleada/markdown-it-text-content'
+import linkAttributes from 'markdown-it-link-attributes'
+import refractor from 'refractor'
+import rehype from 'rehype'
+
+export class Markdownit {
+  constructor (private config: MarkdownIt.Options & { plugins?: ([MarkdownIt.PluginWithOptions, any])[] } = { plugins: [] }) {}
+
+  configure = () => {
+    const { plugins = [], ...options } = this.config,
+          md = new MarkdownIt(options) 
+
+    plugins.forEach(([plugin, options]) => {
+      md.use(plugin, options)
+    })
+
+    return md
+  }
+
+  html () {
+    this.config.html = true
+    return this
+  }
+
+  linkify () {
+    this.config.linkify = true
+    return this
+  }
+
+  highlight (name: 'refractorRehype' | 'refractorRehypeWithEscapedVueMustache') {
+    this.config.highlight = highlightsByName[name]
+    return this
+  }
+
+  plugin (plugin: MarkdownIt.PluginWithOptions, options?: any) {
+    this.config.plugins.push([plugin, options])
+    return this
+  }
+
+  proseContainer (options?: any) {
+    return this.plugin(proseContainer, options)
+  }
+
+  spaLinks (options?: any) {
+    return this.plugin(spaLinks, options)
+  }
+
+  textContent (options?: any) {
+    return this.plugin(textContent, options)
+  }
+
+  linkAttributes (options?: linkAttributes.Config) {
+    return this.plugin(linkAttributes, options)
+  }
+}
+
+const highlightsByName: Record<'refractorRehype' | 'refractorRehypeWithEscapedVueMustache', (str: string, lang: string, attrs: string) => string> = {
+  refractorRehype (code, lang) {
+    try {
+      const children = refractor.highlight(code, lang)
+      
+      return rehype()
+        .stringify({ type: 'root', children })
+        .toString()
+    } catch (error) {
+      return ''
+    }
+  },
+  refractorRehypeWithEscapedVueMustache (code, lang) {
+    try {
+      const children = refractor.highlight(code, lang)
+      
+      return rehype()
+        .stringify({ type: 'root', children })
+        .toString()
+        .replace(/({{)/g, '<span>{</span><span>{</span>')
+        .replace(/(}})/g, '<span>}</span><span>}</span>')
+    } catch (error) {
+      return ''
+    }
+  }
+}
