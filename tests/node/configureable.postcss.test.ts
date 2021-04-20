5e47498472b296ccb9a1dda38eff1f9b69cea6fa
AlexVipond <alex.r.vipond@gmail.com>
2021-04-19 23:06:29 -0600

    tests postcss

--- tests/node/configureable.postcss.test.ts
+++ tests/node/configureable.postcss.test.ts
@@ -1,15 +1,24 @@
 import { suite as createSuite } from 'uvu'
 import * as assert from 'uvu/assert'
 import { Postcss as Configureable } from '../../src/configureables/Postcss'
+import type { PluginCreator } from 'postcss'
 
 const suite = createSuite('configureable.Postcss (node)')
+
+suite(`configures plugins`, context => {
+  const value = new Configureable()
+          .plugin((() => {}) as PluginCreator<any>)
+          .configure()
+  
+  assert.is(value.plugins.length, 1)
+})
 
 suite(`configures import plugin`, context => {
   const value = new Configureable()
           .import()
           .configure()
   
-  assert.ok(value.plugins[0])
+  assert.is(value.plugins.length, 1)
 })
 
 suite(`configures nested plugin`, context => {
@@ -17,7 +26,7 @@ suite(`configures nested plugin`, context => {
           .nested()
           .configure()
   
-  assert.ok(value.plugins[0])
+  assert.is(value.plugins.length, 1)
 })
 
 suite(`configures tailwindcss plugin`, context => {
@@ -25,15 +34,23 @@ suite(`configures tailwindcss plugin`, context => {
           .tailwindcss()
           .configure()
   
-  assert.ok(value.plugins[0])
+  assert.is(value.plugins.length, 1)
 })
 
-suite(`configures presetEnv plugin`, context => {
+suite(`configures autoprefixer plugin`, context => {
   const value = new Configureable()
-          .presetEnv()
+          .autoprefixer()
           .configure()
   
-  assert.ok(value.plugins[0])
+  assert.is(value.plugins.length, 1)
 })
+
+// suite(`configures presetEnv plugin`, context => {
+//   const value = new Configureable()
+//           .presetEnv()
+//           .configure()
+  
+//   assert.is(value.plugins.length, 1)
+// })
 
 suite.run()
