462264dfcc68408debe954519608cd10cb11cd88
AlexVipond <alex.r.vipond@gmail.com>
2022-01-12 23:17:21 -0600

    update prepare script

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,18 +1,10 @@
-import { rollup } from 'rollup'
 import configs from '../rollup.config'
+import { prepare } from '../src/prepare'
 
-async function prepare () {
+async function prepareAll () {
   for (const config of configs) {
-    const bundle = await rollup(config)
-
-    if (!Array.isArray(config.output)) {
-      throw new Error(`config should be an array`)
-    }
-
-    for (const output of config.output) {
-      await bundle.write(output)
-    }
+    await prepare(config)
   }
 }
 
-prepare()
+prepareAll()
